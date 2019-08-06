ee30c62e7e2d83549e049db00195b7b4e6359402
Albert Cheng <lptalbert@gmail.com>
2019-08-06 01:50:24 -0700

    Completed Stack class methods

--- W6D1/stack.rb
+++ W6D1/stack.rb
@@ -1,17 +1,26 @@
 class Stack
-    def initialize
+    def initialize(arr = [])
       # create ivar to store stack here!
+      @stacks = arr
     end
 
     def push(el)
       # adds an element to the stack
+      @stacks.push(el)
     end
 
     def pop
       # removes one element from the stack
+      @stacks.pop
     end
 
     def peek
       # returns, but doesn't remove, the top element in the stack
+      return @stacks[-1]
     end
+
+    private
+
+    attr_reader :stacks
+
 end
