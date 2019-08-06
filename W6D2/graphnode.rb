732cbe3d4255c9a371b8093b22b82f4b152a15b2
Albert Cheng <lptalbert@gmail.com>
2019-08-06 03:17:46 -0700

    Initial commit for GraphNode class

---
+++ W6D2/graphnode.rb
@@ -0,0 +1,13 @@
+class GraphNode
+    attr_accessor :value, :neighbors
+
+    def initialize(value)
+        @value = value
+        @neighbors = []
+    end
+
+    def bfs(starting_node, target_value)
+
+    end
+
+end
