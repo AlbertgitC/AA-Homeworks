0a3b090032f55f59e4f5380aa4ee67901bffeb85
Albert Cheng <lptalbert@gmail.com>
2019-08-07 00:28:18 -0700

    Completed Graphnode class methods

--- W6D2/graphnode.rb
+++ W6D2/graphnode.rb
@@ -13,8 +13,28 @@ class GraphNode
     end
 
     def bfs(starting_node, target_value)
-        
-
+        visited = Set.new()
+        queue = []
+        queue << starting_node
+        until queue.empty?
+            first_node = queue.shift
+            visited.add(first_node)
+            return first_node.value if first_node.value == target_value
+            first_node.neighbors.each { |node| queue << node if visited.none?(node)}
+        end
+        nil
     end
 
 end
+
+
+# a = GraphNode.new('a')
+# b = GraphNode.new('b')
+# c = GraphNode.new('c')
+# d = GraphNode.new('d')
+# e = GraphNode.new('e')
+# f = GraphNode.new('f')
+# a.neighbors = [b, c, e]
+# c.neighbors = [b, d]
+# e.neighbors = [a]
+# f.neighbors = [e]
