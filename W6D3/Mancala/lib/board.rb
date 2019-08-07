22bc5858906260106975de8b77cf4250d7c9d4cb
Albert Cheng <lptalbert@gmail.com>
2019-08-07 03:35:32 -0700

    Completed valid_move? method in 'board.rb'

--- W6D3/Mancala/lib/board.rb
+++ W6D3/Mancala/lib/board.rb
@@ -3,16 +3,20 @@ class Board
 
   def initialize(name1, name2)
     @cups = Array.new(14) { |_| [] }
+    self.place_stones
+  end
+
+  def place_stones
+    # helper method to #initialize every non-store cup with four stones each
     self.cups.each_with_index do |cup, i|
       cup.concat([:stone, :stone, :stone, :stone]) if i != 6 && i != 13
     end
   end
 
-  def place_stones
-    # helper method to #initialize every non-store cup with four stones each
-  end
-
   def valid_move?(start_pos)
+    valid_pos = [0,1,2,3,4,5,7,8,9,10,11,12]
+    raise "Invalid starting cup" if valid_pos.none?(start_pos)
+    raise "Starting cup is empty" if self.cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
