2f7eecc9351f709ea6675d44e053e7b239c8587d
Albert Cheng <lptalbert@gmail.com>
2019-08-07 04:38:01 -0700

    Completed make_move method for 'board.rb'

--- W6D3/Mancala/lib/board.rb
+++ W6D3/Mancala/lib/board.rb
@@ -1,9 +1,12 @@
 class Board
   attr_accessor :cups
+  attr_reader :player1, :player2
 
   def initialize(name1, name2)
     @cups = Array.new(14) { |_| [] }
     self.place_stones
+    @player1 = name1
+    @player2 = name2
   end
 
   def place_stones
@@ -20,10 +23,50 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    stones = self.cups[start_pos].length
+    self.cups[start_pos] = []
+    if current_player_name == self.player1
+      i = 1
+      while stones >= i
+        pos = (start_pos + i) % 14
+        if pos != 13
+          self.cups[pos] << :stone 
+          i += 1
+        else
+          i += 1
+          stones += 1
+        end
+      end
+      self.render
+      self.next_turn((stones + start_pos) % 14)
+    else
+      i = 1
+      while stones >= i
+        pos = (start_pos + i) % 14
+        if pos != 6
+          self.cups[pos] << :stone 
+          i += 1
+        else
+          i += 1
+          stones += 1
+        end
+      end
+      self.render
+      self.next_turn((stones + start_pos) % 14)
+    end
+    
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      return :prompt
+    elsif self.cups[ending_cup_idx].length == 1
+      return :switch 
+    else
+      return ending_cup_idx
+    end
   end
 
   def render
